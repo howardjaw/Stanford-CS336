5e42a337b2a312a004d7922fa0ec4c5c45137d82
Howard Zhao <153261172+howardjaw@users.noreply.github.com>
2026-09-22 22:43:12 -0500

    Read file and split at special tokens

--- assignment1-basics/bpe.py
+++ assignment1-basics/bpe.py
@@ -1,3 +1,4 @@
+import regex as re
 
 """
 Implementation of BPE Tokenizer
@@ -5,6 +6,9 @@ Implementation of BPE Tokenizer
 
 def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
 
+    """
+    Initializing vocab.
+    """
     vocab = {} # Create an empty dictionary for vocab.
 
     merges = [] # Create an empty list for merge history.
@@ -18,7 +22,30 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
     for token in special_tokens:
         token_bytes = token.encode("utf-8")
         vocab[len(vocab)] = token_bytes
+
+    """
+    Reading files and split at special tokens.
+    """
+    with open(input_path, "r", encoding = "utf-8") as f:
+        # Read file.
+        text = f.read()
     
+    chunked = []
+
+    # Split at special tokens.
+    if not special_tokens:
+        chunked.append(text)
+    else:
+        escaped_tokens = []
+        
+        for token in special_tokens:
+            escaped_tokens.append(re.escape(token)) # Using regex to match the literal token.
+        
+        pattern = "|".join(escaped_tokens) # Build a string for all special tokens with "or" for re.split().
+
+        chunked = re.split(pattern, text) # Split the text at special tokens
+    
+
     return vocab, merges
 
 
